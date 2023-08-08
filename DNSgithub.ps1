a0df37506612ecf495b2d9c611ebffc871ccb2e5
khojs <39031496+khojs@users.noreply.github.com>
2023-08-09 02:48:40 +0530

    Add files via upload

---
+++ DNSgithub.ps1
@@ -0,0 +1,3 @@
+﻿
+$ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/khojs/Skype-Ext_DNS_SC/main/LyncDNSTesterTool1.00.ps1?token=GHSAT0AAAAAACGB56BQJ5MU7LXFENIIJXCMZGST5OA
+Invoke-Expression $($ScriptFromGithHub.Content)
